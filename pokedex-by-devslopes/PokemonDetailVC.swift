d3f3955bb546f1362ebfdf85d3cfbbf21e4d4438
Felipe Soares <soaresfelipef@gmail.com>
2016-03-25 20:18:22 -0300

    Detail outlets added'

--- pokedex-by-devslopes/PokemonDetailVC.swift
+++ pokedex-by-devslopes/PokemonDetailVC.swift
@@ -14,12 +14,43 @@ class PokemonDetailVC: UIViewController {
     
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    
+    @IBOutlet weak var descriptionLbl: UILabel!
 
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    @IBOutlet weak var weightLbl: UILabel!
+    
+    @IBOutlet weak var atackLbl: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLbl.text = pokemon.name
         
     }
+    
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
 
 }
